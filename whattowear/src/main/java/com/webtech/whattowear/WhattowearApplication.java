99d9e4429eea89f730ca63d01d68e5e24ed26655
JanLken <109660977+JanLken@users.noreply.github.com>
2023-10-22 22:57:13 +0200

    Initial Commit

---
+++ whattowear/src/main/java/com/webtech/whattowear/WhattowearApplication.java
@@ -0,0 +1,13 @@
+package com.webtech.whattowear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhattowearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhattowearApplication.class, args);
+	}
+
+}
