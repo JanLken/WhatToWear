fef5301661e66c83409dab9c9d99d50c8b5b9793
JLkn <107927104+JLkn@users.noreply.github.com>
2023-10-26 17:57:08 +0200

    Parameter Zugriffe

--- whattowear/src/main/java/com/webtech/whattowear/HelloController.java
+++ whattowear/src/main/java/com/webtech/whattowear/HelloController.java
@@ -1,14 +1,30 @@
 package com.webtech.whattowear;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello() {
         return "Hello World";
     }
+
+    @GetMapping("/user/{userid}")
+    public String userid(@PathVariable int userid) {
+        return "Hello World " + userid;
+    }
+
+    @GetMapping("/user/{userid}/password/{password}")
+    public String useridupassword(@PathVariable int userid, @PathVariable String password) {
+        return "UserID: " + userid + " Password: " + password;
+    }
+
+    @GetMapping("/user")
+    public String queryparam(@RequestParam int userid, @RequestParam(required = false) String password) {
+        return "UserID: " + userid + " Password: " + password;
+    }
+
+
 
 }
