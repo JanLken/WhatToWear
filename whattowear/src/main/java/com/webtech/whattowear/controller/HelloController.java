51056f3631247eb07f2c0225bc86871f09921b37
JLkn <107927104+JLkn@users.noreply.github.com>
2023-10-26 18:49:44 +0200

    Testing mit Datenbank

---
+++ whattowear/src/main/java/com/webtech/whattowear/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.webtech.whattowear.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/user/{userid}")
+    public String userid(@PathVariable int userid) {
+        return "Hello World " + userid;
+    }
+
+    @GetMapping("/user/{userid}/password/{password}")
+    public String useridupassword(@PathVariable int userid, @PathVariable String password) {
+        return "UserID: " + userid + " Password: " + password;
+    }
+
+    @GetMapping("/user")
+    public String queryparam(@RequestParam int userid, @RequestParam(required = false) String password) {
+        return "UserID: " + userid + " Password: " + password;
+    }
+
+
+
+}
