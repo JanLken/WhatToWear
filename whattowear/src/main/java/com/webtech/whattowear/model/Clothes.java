953fdb3ffc7458598973d0b6be4048b0e892ea7d
JLkn <107927104+JLkn@users.noreply.github.com>
2023-11-02 18:04:28 +0100

    Änderungen für Eingabe in DB

--- whattowear/src/main/java/com/webtech/whattowear/model/Clothes.java
+++ whattowear/src/main/java/com/webtech/whattowear/model/Clothes.java
@@ -12,24 +12,25 @@ public class Clothes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    private String name;
-    private boolean waterproof;
-    private String brand;
+    private String category;
+    private Long minTemp;
+    private Long maxTemp;
+
+    private String pathPic;
 
     public Clothes() {}
 
-    public Clothes(String name, boolean waterproof) {
-        this.name = name;
-        this.waterproof = waterproof;
+    public Clothes(String category, Long minTemp, Long maxTemp) {
+        this.category = category;
+        this.maxTemp = maxTemp;
+        this.minTemp = minTemp;
     }
 
     public Integer getId(){ return id;}
 
     public void setId(Integer id) {this.id = id;}
 
-    public String getName() { return name; }
-
-    public void setName(String name) {this.name = name;}
+    public void setCategory(String category) {this.category = category;}
 
     // Constructors, getters, setters, and other methods as needed
 }
