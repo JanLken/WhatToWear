d1b0a6651deb2f5982264bb06d6e7bc80bfd1916
JLkn <107927104+JLkn@users.noreply.github.com>
2023-11-13 10:48:00 +0100

    Ordnung von Ordnern

---
+++ src/main/java/com/webtech/whattowear/repository/ClothesRepository.java
@@ -0,0 +1,9 @@
+package com.webtech.whattowear.repository;
+
+import com.webtech.whattowear.model.Clothes;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClothesRepository extends CrudRepository <Clothes,Long> {
+}
