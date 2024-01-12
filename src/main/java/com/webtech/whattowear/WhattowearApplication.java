53aafde360dfafe9584db3eb622890238bfbb712
JLkn <107927104+JLkn@users.noreply.github.com>
2024-01-12 19:58:41 +0100

    Funktioniert lokal

--- src/main/java/com/webtech/whattowear/WhattowearApplication.java
+++ src/main/java/com/webtech/whattowear/WhattowearApplication.java
@@ -3,8 +3,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-@SpringBootApplication	
-
+@SpringBootApplication
 public class WhattowearApplication {
 
 	public static void main(String[] args) {
